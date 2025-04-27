054902e8ca7ff861effb56684e6ec43cac33a628
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-04-27 23:20:45 +0530

    GPT add

--- src/components/GPTMovieSuggestions.jsx
+++ src/components/GPTMovieSuggestions.jsx
@@ -4,7 +4,7 @@ const GPTMovieSuggestions = () => {
 
     return (
         <div>
-            
+            Movies Suggested
             
         </div>
     )
--- src/components/SearchBarGPT.jsx
+++ src/components/SearchBarGPT.jsx
@@ -4,13 +4,13 @@ const SearchBarGPT = () => {
 
     return (
         <div className="pt-[5%] flex justify-center">
-        <form className="w-1/2 bg-black grid grid-cols-12">
+        <form className="w-3/4 bg-black grid grid-cols-12">
             <input 
             type="text" 
-            className="p-2 m-4 bg-white col-span-9 rounded-lg" placeholder="What would you like to search today?" 
+            className="p-2 m-4 bg-white col-span-10 rounded-lg" placeholder="What would you like to search today?" 
             />
 
-            <button className="m-4 py-2 px-2 bg-red-700 text-white rounded-lg col-span-3">
+            <button className="m-4 py-2 px-2 bg-red-700 text-white rounded-lg col-span-2">
             Search
             </button>
         </form>     
